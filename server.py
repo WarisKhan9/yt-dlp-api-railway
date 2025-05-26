fc1a3e4377ec0bb8d53fa7f8d439d6a0fc195590
waris khan <topupid1976@gmail.com>
2025-05-26 11:15:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -62,7 +62,7 @@ def get_video_info():
 
 
 
-@app.route('/meta', methods=['GET'])
+@app.route('/meta')
 def get_meta():
     url = request.args.get('url')
     if not url:
@@ -94,7 +94,7 @@ def get_meta():
                 'view_count': info.get('view_count'),
                 'like_count': info.get('like_count'),
                 'upload_date': info.get('upload_date'),
-                'thumbnail': thumbnail,
+                # 'thumbnail': thumbnail,
                 'duration': info.get('duration'),
                 'channel_url': info.get('channel_url')
             })
